cd27f791d9d662d9ef557aee27e8ca501d26b07e
Gan Jun Kai <kuhn96@gmail.com>
2020-04-25 16:36:39 +0800

    fix: refined the test case

--- src/lib.rs
+++ src/lib.rs
@@ -4,18 +4,18 @@ use std::sync::Mutex;
 
 pub struct ThreadPool {
     _handles: Vec<std::thread::JoinHandle<()>>,
-    sender: Sender<Box<dyn Fn() + Send>>,
+    sender: Sender<Box<dyn FnMut() + Send>>,
 }
 
 impl ThreadPool {
     pub fn new(num_threads: u8) -> Self {
-        let (sender, receiver) = channel::<Box<dyn Fn() + Send>>();
+        let (sender, receiver) = channel::<Box<dyn FnMut() + Send>>();
         let receiver = Arc::new(Mutex::new(receiver));
         let _handles = (0..num_threads)
             .map(|_| {
                 let clone = receiver.clone();
                 std::thread::spawn(move || loop {
-                    let work = match clone.lock().unwrap().recv() {
+                    let mut work = match clone.lock().unwrap().recv() {
                         Ok(work) => work,
                         Err(_) => break,
                     };
@@ -28,7 +28,7 @@ impl ThreadPool {
         Self { _handles, sender }
     }
 
-    pub fn execute<T: Fn() + Send + 'static>(&self, work: T) {
+    pub fn execute<T: FnMut() + Send + 'static>(&self, work: T) {
         self.sender.send(Box::new(work)).unwrap();
     }
 }
@@ -38,10 +38,19 @@ mod tests {
     use super::*;
     #[test]
     fn it_works() {
+        use std::sync::atomic::{AtomicU32, Ordering};
+
+        let n = AtomicU32::new(0);
+        let nref = Arc::new(n);
+
         let pool = ThreadPool::new(10);
-        let foo = || std::thread::sleep(std::time::Duration::from_secs(1));
+        let clone = nref.clone();
+        let foo = move || {
+            clone.fetch_add(1, Ordering::SeqCst);
+        };
         pool.execute(foo.clone());
-        pool.execute(foo);
-        std::thread::sleep(std::time::Duration::from_secs(2));
+        pool.execute(foo.clone());
+        std::thread::sleep(std::time::Duration::from_secs(1));
+        assert_eq!(nref.load(Ordering::SeqCst), 2);
     }
 }
